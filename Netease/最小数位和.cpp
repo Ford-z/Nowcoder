18c84618098a7834948a6af9fd2f230afe2b8144
Ford-z <51626573+Ford-z@users.noreply.github.com>
2020-09-11 20:33:37 +0800

    Create 最小数位和.cpp
    https://www.nowcoder.com/questionTerminal/4fc97c45a69241e992b3e705e817909c

---
+++ Netease/最小数位和.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<deque>
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+    while (T--){
+        int x,n,m,t;
+        cin >> x;
+        if(x<10){
+            
+            cout<<x<<endl;
+        }
+        else{
+            n=x/9;
+            m=x%9;
+            if(m!=0){
+                cout<<m;
+            }
+            for(int i=0;i<n;i++){
+                cout<<9;
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
